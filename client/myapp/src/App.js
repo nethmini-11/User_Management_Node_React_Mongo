33fdeff9f466cdb9a74b602703e9c2728efaf1cf
nethmini-11 <83303587+nethmini-11@users.noreply.github.com>
2022-07-03 15:51:30 +0530

    Implement frontend navigation and logout

--- client/myapp/src/App.js
+++ client/myapp/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/NavBar";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import PrivateRoute from "./higher_order_component/PrivateRoute";
+import UnPrivateRoute from "./higher_order_component/UnPrivateRoute";
+import Home from "./components/Home";
+import Protect1 from "./components/Protect1";
+import Protect2 from "./components/Protect2";
+import Admin from "./components/Admin";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Router>
+        <NavBar />  
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <PrivateRoute path="/protect1" role={["user", "admin"]} component={Protect1}/>
+          <PrivateRoute path="/protect2" role={["user", "admin"]} component={Protect2}/>
+          <PrivateRoute path="/admin" role={["admin"]} component={Admin}/>
+          <UnPrivateRoute path="/login" component={Login} />
+          <UnPrivateRoute path="/register" component={Register} />
+        </Switch>
+        
+      </Router>
     </div>
   );
 }
